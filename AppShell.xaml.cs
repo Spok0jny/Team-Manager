fbf6f94008c2bde0232383c549d0e9c16588222c
daniel <dankad767@gmail.com>
2024-12-17 02:30:15 +0100

    Updated shell, added flyout in Klub page

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,10 +1,37 @@
-﻿namespace Team_Manager
+﻿using Microsoft.Maui.Controls;
+
+namespace Team_Manager
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+
+            Navigated += OnShellNavigated;
+        }
+
+        private void OnShellNavigated(object sender, ShellNavigatedEventArgs e)
+        {
+  
+            if (e.Current.Location.OriginalString.Contains("Klub"))
+            {
+              
+                FlyoutBehavior = FlyoutBehavior.Flyout;
+                FlyoutMenu.IsVisible = true;
+            }
+            else
+            {
+            
+                FlyoutBehavior = FlyoutBehavior.Disabled;
+                FlyoutMenu.IsVisible = false;
+            }
+        }
+
+
+        private async void OnOsiagnieciaClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new KlubPages.Osiagniecia());
         }
     }
 }
