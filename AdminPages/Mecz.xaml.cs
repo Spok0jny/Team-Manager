8714de05bd5dafc0697ef0dd7afc8b81ea04c84d
[Seweryn Słowik] <sewerynslowik1@gmail.com>
2024-12-26 19:22:29 +0100

    added some fields to the addMatch page (logic still has to be done)

--- AdminPages/Mecz.xaml.cs
+++ AdminPages/Mecz.xaml.cs
@@ -18,14 +18,23 @@ public partial class Mecz : ContentPage
     private async void WczytajZawodnikow()
     {
         ZawodnicyLista = await _dbService.GetZawodnicy();
-        zawodnikPicker.ItemsSource = ZawodnicyLista.Select(z => z.ImieNazwisko).ToList();
+        leftZawodnikPicker.ItemsSource = ZawodnicyLista.Select(z => z.ImieNazwisko).ToList();
+        rightZawodnikPicker.ItemsSource = ZawodnicyLista.Select(z => z.ImieNazwisko).ToList();
     }
 
-    private void OnZawodnikPickerSelectedIndexChanged(object sender, EventArgs e)
+    private void OnLeftZawodnikPickerSelectedIndexChanged(object sender, EventArgs e)
     {
-        if (zawodnikPicker.SelectedIndex != -1)
+        if (leftZawodnikPicker.SelectedIndex != -1)
         {
-            WybranyZawodnikId = ZawodnicyLista[zawodnikPicker.SelectedIndex].Id;
+            WybranyZawodnikId = ZawodnicyLista[leftZawodnikPicker.SelectedIndex].Id;
+        }
+    }
+    
+    private void OnRightZawodnikPickerSelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (rightZawodnikPicker.SelectedIndex != -1)
+        {
+            WybranyZawodnikId = ZawodnicyLista[rightZawodnikPicker.SelectedIndex].Id;
         }
     }
     private async void OnAddEventClicked(object sender, EventArgs e)
@@ -58,5 +67,15 @@ public partial class Mecz : ContentPage
         DataEntry.Date = DateTime.Now;
         druzynaPierwszaEntry.Text = string.Empty;
         druzynaDrugaEntry.Text = string.Empty;
+    }
+
+    private void leftCheckBoxChanged(object sender, CheckedChangedEventArgs e)
+    {
+
+    }
+
+    private void rightCheckBoxChanged(object sender, CheckedChangedEventArgs e)
+    {
+
     }
 }
